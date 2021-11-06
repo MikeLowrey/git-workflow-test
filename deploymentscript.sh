7ab071b79da90ce8127f99d30d96503c69c525bc
martinek <martin@peoplehelper.org>
2021-11-06 20:10:42 +0100

    add deploymentscript

---
+++ deploymentscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# path to local repro folder
+dir_source=$(pwd)
+data=$(date +%A)
+
+echo $dir_source
+echo date
+
+#Variablendefinition
+#suchwort=player
+
+#liste=$(apropos $suchwort)
+#echo "      Player-Liste:"
+#echo "$liste"
+npm -v
